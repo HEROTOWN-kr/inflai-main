aeb143e1da20b9cc1ee2d085bd6b11e8acb28e4a
Andrian <andriantsoy@gmail.com>
2020-12-31 09:21:26 +0900

    배경 병경

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -8,13 +8,14 @@ function App() {
         <div className="intro-content">
           <div className="intro-fake" />
           <div className="intro-area">
+            <div className="intro-logo" />
             <div className="intro-title">인플루언서이신가요?</div>
             <div className="intro-meta">
-              <div className="intro-text">내가 원하는 브랜드 협찬! 당신을 위한 협찬 매니저</div>
+              <div className="intro-text">내가 찾던 브랜드 협찬. 당신만의 협찬 파트너</div>
               <div className="intro-hashtag">#인플라이</div>
             </div>
-            <a target="_blank" href="https://influencer.inflai.com" className="intro-btn">
-              <span className="intro-btn__text">서비스 알아보기</span>
+            <a href="https://influencer.inflai.com" className="intro-btn">
+              <span className="intro-btn__text">지금 알아보기</span>
             </a>
           </div>
         </div>
@@ -24,14 +25,15 @@ function App() {
         <div className="intro-content">
           <div className="intro-fake" />
           <div className="intro-area">
+            <div className="intro-logo" />
             <div className="intro-title">광고주이신가요?</div>
             <div className="intro-meta">
-              <div className="intro-text">가장 빠른 인플루언서 매칭 완료</div>
-              <div className="intro-text">상세한 실시간 성과분석 리포트</div>
-              <div className="intro-text">가장 합리적인 비용산출 시스템</div>
+              <div className="intro-text">내 예산에 가장 효과적인 마케팅</div>
+              <div className="intro-text">쉽고 빠른 인플루언서 매칭</div>
+              <div className="intro-text">블로그, 인스타, 유튜브, 틱톡까지</div>
             </div>
-            <a target="_blank" href="https://biz.inflai.com" className="intro-btn">
-              <span className="intro-btn__text">서비스 알아보기</span>
+            <a href="https://biz.inflai.com" className="intro-btn">
+              <span className="intro-btn__text">지금 알아보기</span>
             </a>
           </div>
         </div>
