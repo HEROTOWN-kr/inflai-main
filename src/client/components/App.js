6f34cdfc39d7db4494ec1636629bdd9b3b99cf95
Andrian <andriantsoy@gmail.com>
2021-01-07 21:49:10 +0900

    배경 병경

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+function checkProduction(hostname) {
+  const result = hostname.indexOf('inflai.com');
+  return result !== -1;
+}
+
 function App() {
+  const isProduction = checkProduction(window.location.origin);
+  const BizPage = isProduction ? 'https://biz.inflai.com' : 'http://localhost:3003';
+  const InfPage = isProduction ? 'https://influencer.inflai.com' : 'http://localhost:3002';
+
   return (
     <div className="intro">
       <div className="intro-col intro-col--left">
@@ -9,12 +18,12 @@ function App() {
           <div className="intro-fake" />
           <div className="intro-area">
             <div className="intro-logo" />
-            <div className="intro-title">인플루언서이신가요?</div>
+            <div className="intro-title">인플루언서</div>
             <div className="intro-meta">
               <div className="intro-text">내가 찾던 브랜드 협찬. 당신만의 협찬 파트너</div>
               <div className="intro-hashtag">#인플라이</div>
             </div>
-            <a href="https://influencer.inflai.com" className="intro-btn">
+            <a href={InfPage} className="intro-btn">
               <span className="intro-btn__text">지금 알아보기</span>
             </a>
           </div>
@@ -26,13 +35,13 @@ function App() {
           <div className="intro-fake" />
           <div className="intro-area">
             <div className="intro-logo" />
-            <div className="intro-title">광고주이신가요?</div>
+            <div className="intro-title">광고주</div>
             <div className="intro-meta">
               <div className="intro-text">내 예산에 가장 효과적인 마케팅</div>
               <div className="intro-text">쉽고 빠른 인플루언서 매칭</div>
               <div className="intro-text">블로그, 인스타, 유튜브, 틱톡까지</div>
             </div>
-            <a href="https://biz.inflai.com" className="intro-btn">
+            <a href={BizPage} className="intro-btn">
               <span className="intro-btn__text">지금 알아보기</span>
             </a>
           </div>
